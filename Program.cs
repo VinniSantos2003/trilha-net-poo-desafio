0dc1264d5fb983d8704ccb05f4bee6077dbf06e6
Vinni <vinixreduzino@gmail.com>
2025-06-24 16:40:27 -0300

    TODO feitos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia\n");
+
+Nokia n = new Nokia("22987654321", "Aphla_01", "0000000000", 256);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nTestando Iphone\n");
+
+Iphone i = new Iphone("22123456789", "Iphone PRO MAX 18" , "1111111111", 1024);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Telegram");
